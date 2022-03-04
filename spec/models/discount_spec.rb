8821808ee9822c10d6810761ab400a254c031886
Aedan Yturralde <aedanyturralde@gmail.com>
2022-03-03 18:45:38 -0700

    test: write tests for discount model relationships and validations. Add validations and relationships to model, pass tests

---
+++ spec/models/discount_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe Discount, type: :model do 
+  describe 'relationships' do 
+    it {should belong_to :merchant}
+    it {should have_many(:items).through(:merchant)}
+    it {should have_many(:invoice_items).through(:items)}
+    it {should have_many(:invoices).through(:invoice_items)}
+ end 
+
+  describe 'validations' do 
+    it {should validate_presence_of :name}
+    it {should validate_presence_of :threshold}
+    it {should validate_numericality_of(:percentage).only_integer}
+    it {should validate_numericality_of(:percentage).is_less_than_or_equal_to(100)}
+    it {should validate_numericality_of(:threshold).only_integer}
+  end 
+end 
