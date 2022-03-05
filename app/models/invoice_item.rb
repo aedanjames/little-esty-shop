cbe7ccb5a15a3b48e3ceda4010acc8d06e4567b2
Aedan Yturralde <aedanyturralde@gmail.com>
2022-03-05 09:46:02 -0700

    add active record has_many relationship to invoice item model. Write test, pass test

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -3,6 +3,7 @@ class InvoiceItem < ApplicationRecord
   belongs_to :invoice
   belongs_to :item
   has_many :merchants, through: :item
+  has_many :discounts, through: :merchants
 
   def get_name_from_invoice
     item.name
--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe InvoiceItem, type: :model do
     it {should belong_to(:item)}
     it {should belong_to(:invoice)}
     it {should have_many(:merchants).through(:item)}
+    it {should have_many(:discounts).through(:merchants)}
   end
 
   describe 'validations' do
