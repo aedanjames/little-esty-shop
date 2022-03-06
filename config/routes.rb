77ecf9fda0aeea7a1abe070d6b13c271e8e85c0a
Aedan Yturralde <aedanyturralde@gmail.com>
2022-03-05 17:16:37 -0700

    add route resource for merchants index. Create merchants controlller with index action. Create index view, write test for index view displays merchant names

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
   Rails.application.routes.draw do
 
-  get '/merchants', to: 'merchants#index'
   get '/', to: 'application#welcome'
-    resources :merchants, only: [:show, :index, :update, :post], module: :merchant do
-      resources :discounts, only: [:index, :show, :edit, :update]
-      resources :invoices, only: [:index, :show, :update]
-      resources :dashboard, only: [:index]
-      resources :items, only: [:index, :show, :edit, :update, :new, :create]
-    end
-
-    namespace :admin, only: [:index, :show, :edit, :update, :new, :create], module: :admin do
-      resources :dashboard, only: [:index]
-      resources :merchants, only: [:index, :show, :edit, :update, :new, :create]
-      resources :invoices, only: [:index, :show, :update]
-    end
+  resources :merchants, only: [:index]
+  
+  resources :merchants, only: [:show, :index, :update, :post], module: :merchant do
+    resources :discounts, only: [:index, :show, :edit, :update]
+    resources :invoices, only: [:index, :show, :update]
+    resources :dashboard, only: [:index]
+    resources :items, only: [:index, :show, :edit, :update, :new, :create]
+  end
+  namespace :admin, only: [:index, :show, :edit, :update, :new, :create], module: :admin do
+    resources :dashboard, only: [:index]
+    resources :merchants, only: [:index, :show, :edit, :update, :new, :create]
+    resources :invoices, only: [:index, :show, :update]
+  end
 end
